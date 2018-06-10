bfd6c79dc9dd688904e2b8e726c4c6281420ec00
jbockerstette <jim.bockerstette@gmail.com>
2018-06-10 09:10:32 -0700

    Added some tests

--- src/App.js
+++ src/App.js
@@ -16,49 +16,63 @@ const colors = [
   '#73A857'
 ];
 
-const Button = props => <button className="btn" {...props} />;
-const ButtonLink = ({ className, ...rest }) => {
+export const Grid = ({ bgColor, ...rest }) => (
+  <div className="grid" style={{ background: bgColor }} {...rest} />
+);
+export const Button = ({ bgColor, ...rest }) => (
+  <button className="btn" style={{ background: bgColor }} {...rest} />
+);
+export const ButtonLink = ({ className, bgColor, ...rest }) => {
   const cls = ['btn', className].join(' ');
-  return <a className={cls} {...rest} />;
+  return <a className={cls} style={{ background: bgColor }} {...rest} />;
 };
 
-const Quote = ({ quote, ...rest }) => (
-  <p {...rest}>
+export const Quote = ({ quote, color, opacity, ...rest }) => (
+  <p {...rest} style={{ color, opacity }}>
     <i className="fa fa-quote-left" />
     {` ${quote} `}
     <i className="fa fa-quote-right" />
   </p>
 );
 
-const Author = ({ author, ...rest }) => <p {...rest}>{`- ${author}`}</p>;
+export const Author = ({ author, color, opacity, ...rest }) => (
+  <p {...rest} style={{ color, opacity }}>{`- ${author}`}</p>
+);
 
 class App extends React.Component {
-  state = { quotes: [], quote: 0, colorIndex: 0, opacity: 0 };
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [{ quote: '', author: '' }],
+      quote: 0,
+      colorIndex: 0,
+      opacity: 0
+    };
+    this.handleNextQuote = this.handleNextQuote.bind(this);
+  }
+
   async componentDidMount() {
     try {
       const json = await fetch(
         'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
       );
       const quotes = await json.json();
-      this.setState(
-        () => ({ quotes: quotes.quotes }),
-        () => this.handleNextQuote()
-      );
+      this.setState(() => ({ quotes: quotes.quotes }));
     } catch (error) {
       console.error(error);
     }
   }
 
-  getTwitterLink = q => {
+  static getTwitterLink(q) {
     if (q) {
       return `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='${encodeURIComponent(
         `"${q.quote}" ${q.author}`
       )}`;
     }
     return '';
-  };
+  }
 
-  handleNextQuote = () => {
+  handleNextQuote() {
     this.setState(() => ({ opacity: 0 }));
     setTimeout(() => {
       const quote = Math.round(Math.random() * this.state.quotes.length);
@@ -69,33 +83,36 @@ class App extends React.Component {
         }
         return { quote, colorIndex: nextColor, opacity: 1 };
       });
-    }, 1500);
-  };
+    }, 0);
+  }
 
   render() {
     const { quotes, quote, colorIndex, opacity } = this.state;
     const q = quotes[quote];
-    const bgColor = { background: colors[colorIndex] };
-    const color = { color: colors[colorIndex], opacity };
-    const href = this.getTwitterLink(q);
-
+    const color = colors[colorIndex];
+    const href = App.getTwitterLink(q);
     return q ? (
-      <div className="grid" style={bgColor}>
+      <Grid bgColor={color}>
         <div id="quote-box">
-          <Quote id="text" style={color} quote={q.quote} />
-          <Author id="author" style={color} author={q.author} />
+          <Quote id="text" color={color} opacity={opacity} quote={q.quote} />
+          <Author
+            id="author"
+            color={color}
+            opacity={opacity}
+            author={q.author}
+          />
           <ButtonLink
             className="fa fa-twitter"
             id="tweet-quote"
-            style={bgColor}
+            bgColor={color}
             href={href}
             target="_blank"
           />
-          <Button id="new-quote" onClick={this.handleNextQuote} style={bgColor}>
+          <Button id="new-quote" onClick={this.handleNextQuote} bgColor={color}>
             New Quote
           </Button>
         </div>
-      </div>
+      </Grid>
     ) : null;
   }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,107 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { mount } from 'enzyme';
+import App, { Grid } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const quotes = [
+  {
+    quote: 'Hello World',
+    author: 'Jim'
+  }
+];
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+// global.fetch = () => Promise.resolve(new Response(JSON.stringify({ quotes })));
+
+describe('App', () => {
+  let props;
+  let mountedApp;
+  let spy;
+  fetch.mockResponse(JSON.stringify({ quotes }));
+  const app = async () => {
+    if (!mountedApp) {
+      spy = jest.spyOn(App.prototype, 'handleNextQuote');
+      mountedApp = mount(<App />);
+      await mountedApp.instance().componentDidMount();
+    }
+    return mountedApp;
+  };
+
+  beforeEach(() => {
+    props = {};
+    mountedApp = undefined;
+  });
+
+  // All tests go here.
+  it('renders the correct components', async () => {
+    const app1 = await app();
+    const divs = app1.find('div');
+    const grid = app1.find('Grid');
+    const button = app1.find('Button');
+    const link = app1.find('ButtonLink');
+    const quote = app1.find('Quote');
+    const author = app1.find('Author');
+    expect(divs.length).toBeGreaterThan(0);
+    expect(grid.length).toBeGreaterThan(0);
+    expect(button.length).toBeGreaterThan(0);
+    expect(link.length).toBeGreaterThan(0);
+    expect(quote.length).toBeGreaterThan(0);
+    expect(author.length).toBeGreaterThan(0);
+  });
+  it('always sets correct state from mock', async () => {
+    const app1 = await app();
+    const { state } = app1.instance();
+    expect(state.quotes.length).toBeGreaterThan(0);
+    expect(state.quotes).toMatchObject(quotes);
+  });
+
+  it('should have the correct Author', async () => {
+    const app1 = await app();
+    const author = app1.find('Author');
+    expect(author.get(0).props).toHaveProperty('id', 'author');
+    expect(author.get(0).props).toHaveProperty('author', '');
+  });
+  it('should have the correct Quote', async () => {
+    const app1 = await app();
+    const quote = app1.find('Quote');
+    expect(quote.get(0).props).toHaveProperty('id', 'text');
+    expect(quote.get(0).props).toHaveProperty('quote', '');
+  });
+  it('should have the correct ButtonLink', async () => {
+    const app1 = await app();
+    const bl = app1.find('ButtonLink');
+    expect(bl.get(0).props).toHaveProperty('id', 'tweet-quote');
+  });
+  it('should have the correct Button', async () => {
+    const app1 = await app();
+    const btn = app1.find('Button');
+    expect(btn.get(0).props).toHaveProperty('id', 'new-quote');
+    expect(spy).toHaveBeenCalledTimes(0);
+    btn.simulate('click');
+    expect(spy).toHaveBeenCalledTimes(1);
+    // expect(app1.find('#text').get(0).props).toHaveProperty('opacity', 0);
+  });
+  it('should have the correct quote and author after click', async done => {
+    const app1 = await app();
+    app1.setState({ quotes: [{ quote: 'hi', author: 'jim' }] }, () => {
+      const author = app1.find('Author');
+      const quote = app1.find('Quote');
+      expect(author.get(0).props).toHaveProperty('author', 'jim');
+      expect(quote.get(0).props).toHaveProperty('quote', 'hi');
+      done();
+    });
+    // expect(app1.find('#text').get(0).props).toHaveProperty('opacity', 0);
+  });
+});
+
+describe('Grid', () => {
+  const mountedGrid = mount(<Grid bgColor="red" />);
+  it('always renders a div', () => {
+    const divs = mountedGrid.find('div');
+    expect(divs.length).toBeGreaterThan(0);
+  });
+  it('should set the bg color style from props', () => {
+    const divs = mountedGrid.find('div');
+    expect(divs.get(0).props.style).toHaveProperty('background', 'red');
+  });
 });
